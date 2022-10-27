9eaa4475683dd1d1861a4e190330e045bc2e4d4e
Michael Sjöberg <random@michaelsjoberg.com>
2022-10-28 01:06:25 +0200

    Create eng.py

---
+++ lang/eng.py
@@ -0,0 +1,55 @@
+# lang : english
+OPERATORS = {
+    'plus': '+',
+    'add': '+',
+    'added': '+',
+    'minus': '-',
+    'subtract': '-',
+    'subtracted': '-',
+    'greater': '>',
+    'more': '>',
+    'larger': '>',
+    'less': '<',
+    'smaller': '<',
+    'divided': '/',
+    'split': '/',
+    'times': '*',
+    'multiply': '*',
+    'multiplied': '*',
+}
+NUMBERS = {
+    'zero': 0,
+    'one': 1,
+    'two': 2,
+    'tree': 3,
+    'four': 4,
+    'five': 5,
+    'six': 6,
+    'seven': 7,
+    'eight': 8,
+    'nine': 9,
+    'ten': 10,
+    'eleven': 11,
+    'twelve': 12,
+    'thirteen': 13,
+    'fourteen': 14,
+    'fifteen': 15,
+    'sixteen': 16,
+    'seventeen': 17,
+    'eighteen': 18,
+    'nineteen': 19,
+    'twenty': 20
+}
+BOOLEAN = {
+    True: 'True',
+    False: 'False',
+}
+TEST_MAP = {
+    'one plus one': 2,
+    'two minus tree': -1,
+    'five ants are more than four elephants': BOOLEAN[True],
+    'two apples split on two people': 1,
+    'tree pears divided with two people': 1,
+    'one times two minus two times four': -6,
+}
+# ./lang
